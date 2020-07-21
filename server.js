be6aee5f047c281ab727abc19553ff85f383a168
brandonolin <olin.brandon@gmail.com>
2020-07-21 10:05:52 -0600

    Port variable created to work in development, or production with port provided by heroku

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const articleRouter = require('./routers/article');
-const userRouter = require('./routers/user');
+const express = require('express')
+const mongoose = require('mongoose')
+const articleRouter = require('./routers/article')
+const userRouter = require('./routers/user')
 
-const app = express();
+const app = express()
+const port = process.env.PORT || 3000
 
 // Database
 mongoose
-  .connect("mongodb://127.0.0.1:27017/simple-blog-db", {
+  .connect('mongodb://127.0.0.1:27017/simple-blog-db', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-    useFindAndModify: false,
+    useFindAndModify: false
   })
-  .then(() => console.log("Connected to database"))
-  .catch((err) => console.log(err));
+  .then(() => console.log('Connected to database'))
+  .catch((err) => console.log(err))
 
 // Middleware
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 // Routers
-app.use(articleRouter);
-app.use(userRouter);
+app.use(articleRouter)
+app.use(userRouter)
 
 // Start Server
-app.listen(3000, () => console.log("Server started on port 3000"));
+app.listen(port, () => console.log(`Server started on port ${port}`))
