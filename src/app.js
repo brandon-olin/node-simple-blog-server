b23f3d1cff365b36af7c3167ee5f4439dc7499ea
brandonolin <olin.brandon@gmail.com>
2020-07-21 12:39:41 -0600

    reverting back to env file

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const port = process.env.PORT
 
 // Database
 mongoose
-  .connect('mongodb://127.0.0.1:27017/simple-blog-api', {
+  .connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
